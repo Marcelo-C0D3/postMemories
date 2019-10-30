794a9e8d21581c22e182e8d87d094e22e539c595
kingC0D3R <marceloloch.newaa@gmail.com>
2019-10-30 11:44:31 -0300

    Hehe

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Gravatar } from 'react-native-gravatar'
 import {
     StyleSheet,
     Text,
@@ -10,11 +12,20 @@ import icon from '../../assets/imgs/icon.png'
 
 class Header extends Component {
     render() {
+        const name = this.props.name || 'Anonymous'
+        const gravatar = this.props.email ?
+            <Gravatar options={{ email: this.props.email, secure: true }}
+                style={styles.avatar} />
+            : null
         return (
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
                     <Image source={icon} style={styles.image} />
-                    <Text style={styles.title}>Post Your Memories</Text>
+                    <Text style={styles.title}>Post Your Memorie</Text>
+                </View>
+                <View style={styles.userContainer}>
+                    <Text style={styles.user}>{name}</Text>
+                    {gravatar}
                 </View>
             </View>
         )
@@ -25,8 +36,11 @@ const styles = StyleSheet.create({
     container: {
         marginTop: Platform.OS === 'ios' ? 20 : 0,
         padding: 8,
-        borderBottomWidth: 0.7,
+        borderBottomWidth: 1,
         borderColor: '#BBB',
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     rowContainer: {
         flexDirection: 'row',
@@ -40,9 +54,31 @@ const styles = StyleSheet.create({
     title: {
         color: '#000',
         fontFamily: 'shelter',
-        paddingLeft: 25,
+        paddingLeft: 20,
         height: 34,
         fontSize: 26
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    user: {
+        fontSize: 10,
+        color: '#888',
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
     }
 })
-export default Header
+
+const mapStateToProps = ({ user }) => {
+    return {
+        email: user.email,
+        name: user.name,
+    }
+}
+
+// export default Header
+export default connect(mapStateToProps)(Header)
